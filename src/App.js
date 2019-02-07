de8c3a03cda42ec7034ecab6b6bf1d9018858f0e
Casey Erbes <casey@erbes.com>
2019-02-07 17:50:39 -0600

    Adding puzzle tiles

--- src/App.js
+++ src/App.js
@@ -27,6 +27,54 @@ class App extends Component {
               A Small Puzzle
             </Typography>
             <div className="Puzzle-box">
+              <div className="Inner-puzzle-track">
+                <div className="Inner-puzzle-box"></div>
+                <div className="Puzzle-tile Puzzle-row-1 Puzzle-col-1">
+                  <span>1</span>
+                </div>
+                <div className="Puzzle-tile Puzzle-row-1 Puzzle-col-2">
+                  <span>2</span>
+                </div>
+                <div className="Puzzle-tile Puzzle-row-1 Puzzle-col-3">
+                  <span>3</span>
+                </div>
+                <div className="Puzzle-tile Puzzle-row-1 Puzzle-col-4">
+                  <span>4</span>
+                </div>
+                <div className="Puzzle-tile Puzzle-row-2 Puzzle-col-1">
+                  <span>5</span>
+                </div>
+                <div className="Puzzle-tile Puzzle-row-2 Puzzle-col-2">
+                  <span>6</span>
+                </div>
+                <div className="Puzzle-tile Puzzle-row-2 Puzzle-col-3">
+                  <span>7</span>
+                </div>
+                <div className="Puzzle-tile Puzzle-row-2 Puzzle-col-4">
+                  <span>8</span>
+                </div>
+                <div className="Puzzle-tile Puzzle-row-3 Puzzle-col-1">
+                  <span>9</span>
+                </div>
+                <div className="Puzzle-tile Puzzle-row-3 Puzzle-col-2">
+                  <span>10</span>
+                </div>
+                <div className="Puzzle-tile Puzzle-row-3 Puzzle-col-3">
+                  <span>11</span>
+                </div>
+                <div className="Puzzle-tile Puzzle-row-3 Puzzle-col-4">
+                  <span>12</span>
+                </div>
+                <div className="Puzzle-tile Puzzle-row-4 Puzzle-col-1">
+                  <span>13</span>
+                </div>
+                <div className="Puzzle-tile Puzzle-row-4 Puzzle-col-2">
+                  <span>14</span>
+                </div>
+                <div className="Puzzle-tile Puzzle-row-4 Puzzle-col-3">
+                  <span>15</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
